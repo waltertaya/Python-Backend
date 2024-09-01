640a4778c39ca92081965070b708b614b13266b3
waltertaya <waltermittybecker5@gmail.com>
2024-09-01 12:08:01 +0300

    Pymongo flask connection

---
+++ NoSQL Databases/Document DBs/app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from pymongo import MongoClient
+
+def connect_db():
+    uri = 'mongodb+srv://<username>:<password>@waltertayadb.y2nbk2w.mongodb.net/'
+    try:
+        client = MongoClient(uri)
+        # db = client['movies-db']
+        db = client.movies_db
+        print('Databases connected successfully')
+        return db
+    except Exception as e:
+        raise e
+
+
+def create_app():
+    app = Flask(__name__)
+    with app.app_context():
+        db = connect_db()
+        app.config['db'] = db
+    return app
